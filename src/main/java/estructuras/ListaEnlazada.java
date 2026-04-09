1363a5ad34790757fc5266b311d852092bce7833
Juan Jose <juanjomotapears@gmail.com>
2026-04-09 13:47:21 -0500

    esqueleto del proyecto

---
+++ src/main/java/estructuras/ListaEnlazada.java
@@ -0,0 +1,23 @@
+package estructuras;
+
+public class ListaEnlazada<T> {
+
+    private Nodo<T> first;
+    private Nodo<T> last;
+
+    public boolean add(){
+        return true;
+    }
+
+    public boolean getData(){
+        return true;
+    }
+
+    public boolean search(){
+        return true;
+    }
+
+    public boolean delete(){
+        return true;
+    }
+}
